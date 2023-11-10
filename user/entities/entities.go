a35c845ddb099b982877a6481a69e8143e13e6cb
RafalFDS2 <rafael.ferraz@picpay.com>
2023-11-10 18:10:25 -0300

    Base entitites

--- user/entities/entities.go
+++ user/entities/entities.go
@@ -1,5 +1,18 @@
 package entities
 
+import "time"
+
 type Store interface {}
 
 type Service interface {}
+
+type User struct {
+	UserId string `json:"userId"`
+	Saldo float64 `json:"saldo"`
+	CreatedAt *time.Time `json:"createdAt"`
+	IsActive bool `json:"isActive"`
+}
+
+type UserPatchRequest struct {
+	IsActive *bool `json:"isActive"`
+}
